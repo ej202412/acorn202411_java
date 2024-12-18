4f9ecb880f60f473b8208bfe492af9d3ed9c8db3
오은지 <ysu2019@yonsei.ac.kr>
2024-12-18 11:45:07 +0900

    Step 07 에 MainClass05 완성함

--- Step07_Array/src/test/main/MainClass05.java
+++ Step07_Array/src/test/main/MainClass05.java
@@ -11,6 +11,25 @@ public class MainClass05 {
 		 *  3. 반복문 for 를 이용해서 순서대로 배열의 각각의 방에 있는 Bike 객체의
 		 *     ride() 메소드를 호출해 보세요.
 		 */
-
+		//1.
+		Bike[] bikes=new Bike[3];
+		//배열의 방의 사이즈만큼 반복문 돌기 	
+		for(int i=0; i<bikes.length; i++) {
+			//2.
+			bikes[i]=new Bike();		
+		}
+		//3.
+		for(int i=0; i<bikes.length; i++) {
+			//i 번째 방에 있는 Bike 객체의 참조값 얻어오기
+			Bike tmp=bikes[i];
+			//tmp 에 있는 Bike 객체의 .ride() 메소드 호출하기
+			tmp.ride();		
+		}
+		
+		System.out.println();
+		
+		for(Bike tmp:bikes) {
+			tmp.ride();
+		}
 	}
 }
