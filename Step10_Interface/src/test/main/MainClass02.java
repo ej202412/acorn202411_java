17568da11ded38f2f661a36258570312095ef59b
오은지 <ysu2019@yonsei.ac.kr>
2024-12-20 14:43:38 +0900

    Step10 예제 완성함

---
+++ Step10_Interface/src/test/main/MainClass02.java
@@ -0,0 +1,24 @@
+package test.main;
+
+import test.mypac.MyRemocon;
+import test.mypac.Remocon;
+
+public class MainClass02 {
+	public static void main(String[] args) {
+		//다형성 확인 
+		MyRemocon r1=new MyRemocon();
+		Remocon r2=r1;
+		Object r3=r1;
+		
+		useRemocon(r1);
+		useRemocon(r2);
+		//casting 해서 전달하면 메소드를 호출할 수 있다.
+		useRemocon((Remocon)r3); //MyRemocon 타입으로 캐스팅도 가능
+		
+	}
+	
+	public static void useRemocon(Remocon r) {
+		r.up();
+		r.down();
+	}
+}
